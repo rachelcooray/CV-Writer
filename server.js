0bc075647fff995498cc41e830cc6b3bb5d38c16
rachelcooray <rachelcooray@gmail.com>
2026-01-05 20:44:29 +0530

    Fix Express 5 wildcard route syntax

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.post('/api/analyze', async (req, res) => {
 });
 
 // Serve React App for any other route
-app.get('*', (req, res) => {
+app.get(/(.*)/, (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
